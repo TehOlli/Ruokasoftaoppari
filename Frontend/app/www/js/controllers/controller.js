ed4117f54ec8ce874e4903a25377b82910ba9923
TehOlli <onyholm94@gmail.com>
2017-03-20 16:38:38 +0200

    saving token to storage, splash screen and validations

--- Frontend/app/www/js/controllers/controller.js
+++ Frontend/app/www/js/controllers/controller.js
@@ -1,31 +1,65 @@
 app.controller('signupController', ['$scope', '$log', '$http', function($scope, $log, $http) {
+
        $scope.userName = "";
-       $scope.email = ""
+       $scope.email = "";
+       $scope.emailVal = /^(([^<>()[\]\\.,;:\s@\']+(\.[^<>()[\]\\.,;:\s@\']+)*)|(\'.+\'))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zåäöA-ZÅÄÖ\-0-9]+\.)+[a-zåäöA-ZÅÄÖ]{2,}))$/;
+       $scope.nameVal = /^[a-zåäö0-9]+$/i;
+       
+       document.addEventListener('init', function (e) { 
+            if (e.target.id == 'signup') { 
+                 if(localStorage.token != null){
+                    myNavigator
+                    .pushPage("list.html", {animation : 'none'})
+                }
+
+            }
+            });
+
        $scope.inputFunction = function(){
+
             if($scope.userName==""||$scope.email==""){
+
                 ons.notification.alert("Fill in the information");
 
             }
+
+            else if(!$scope.signupForm.signupInput.$valid){
+                ons.notification.alert("Enter a proper email address");
+                
+            }
+
+            else if(!$scope.signupForm.signupInput2.$valid){
+                ons.notification.alert("Username can only contain letters and numbers");
+            }
+
             else{
                 var data = JSON.stringify({username:$scope.userName, email:$scope.email});
+                $log.info(data);
 
                  $http.post('http://localhost:8080/signup', data).then(function (success){
-                    $log.info("success", success);
+
+                    localStorage.token = success;
+                    $log.info("success", localStorage.token);
+
+                    myNavigator
+                    .pushPage("list.html", {})
+
                 },function (error){
                     $log.info("error", error)
                 });
 
-                $log.info(data);
-                 myNavigator
-                .pushPage("list.html", {
-    
-                })
             }
        }
 
 }]);
 
 app.controller('listController', ['$scope', '$log', function($scope, $log) {
+    
+    $scope.removeToken = function(){
+        $log.info("token removed")
+        localStorage.removeItem("token");
+       }
+
       $scope.groups = [
           {
               name: 'Topis group',
