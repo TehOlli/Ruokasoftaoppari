c07c0d6de0f5ce0fd78fb48d768a56e39579ddb9
TehOlli <onyholm94@gmail.com>
2017-03-24 13:48:23 +0200

    email header

--- Frontend/ruokasofta/www/js/app.js
+++ Frontend/ruokasofta/www/js/app.js
@@ -5,6 +5,7 @@ function Interceptor() {
     request: function(config) {
         if(localStorage.token != null){
         config.headers['Authorization'] = 'Bearer ' + localStorage.token;
+        config.headers['Email'] = localStorage.email;
         console.log(config.headers);
         }
       return config;
