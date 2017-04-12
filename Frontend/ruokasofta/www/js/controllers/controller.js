8fbb7891ddb17adf19bc7091342b70dbe228af74
TehOlli <onyholm94@gmail.com>
2017-04-12 15:23:33 +0300

    chat date

--- Frontend/ruokasofta/www/js/controllers/controller.js
+++ Frontend/ruokasofta/www/js/controllers/controller.js
@@ -178,9 +178,12 @@ app.controller('createController', ['$scope', '$log', '$http', function($scope, 
 app.controller('groupController', ['$scope', '$log', '$http', '$anchorScroll', function($scope, $log, $http, $anchorScroll) {
     $scope.chatInput = "";
     $scope.messages = [];
+    $scope.msgdate = "01.01.1970";
+    $scope.date = false;
     var local = "http://localhost:8080/";
     var proto = "http://proto453.haaga-helia.fi:80/";
     var id = {id: localStorage.id};
+
     $http.get(local + 'auth/getmessages', {headers: id}).then(function (success){
 
                 $log.info(success.data);
@@ -201,10 +204,10 @@ app.controller('groupController', ['$scope', '$log', '$http', '$anchorScroll', f
     $scope.sendmesFunction = function(){
         if(!$scope.chatInput==""){
             var t = new Date();
-            var time = t.getHours() + "." + t.getMinutes();
-            var date = t.getDate() + "." + t.getMonth() + "." + t.getFullYear(); 
+            var time = ('0' + t.getHours()).slice(-2) + "." + ('0' + t.getMinutes()).slice(-2);
+            var date = ('0' + t.getDate()).slice(-2) + "." + ('0' + (t.getMonth()+1)).slice(-2) + "." + t.getFullYear(); 
             console.log(time, date);
-            $scope.messages.push({username: localStorage.name, msg: $scope.chatInput, own:true, time:time});
+            $scope.messages.push({username: localStorage.name, msg: $scope.chatInput, own:true, time:time, date:date});
             socket.emit('message', {room: localStorage.id, msg:$scope.chatInput, username:localStorage.name, email:localStorage.email, time:time, date:date});
             $scope.chatInput = "";
 
