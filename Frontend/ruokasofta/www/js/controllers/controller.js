3608ab4778b51aee541021412f53a1209a16082f
TehOlli <onyholm94@gmail.com>
2017-04-26 16:33:43 +0300

    get closest restaurants and initial search

--- Frontend/ruokasofta/www/js/controllers/controller.js
+++ Frontend/ruokasofta/www/js/controllers/controller.js
@@ -339,27 +339,102 @@ app.controller('mapController', ['$scope', '$log', '$http', '$timeout', function
  
     var local = "http://localhost:8080/";
     var proto = "http://proto453.haaga-helia.fi:80/";
+    $scope.list = true;
+    $scope.close = false;
     
+
     document.addEventListener('init', function (e) { 
 
             if (e.target.id == 'mappage') {
 
+                $scope.$watch('close', function(newValue){
+                    if(!newValue){
+                        clearMarkers(1);
+                    }
+                    if(newValue){
+                        showMarkers(1);
+                    }
+                })
+                $scope.$watch('list', function(newValue){
+                    if(!newValue){
+                        clearMarkers(2);
+                    }
+                    if(newValue){
+                        showMarkers(2);
+                    }
+                })
+                var markers = [];
+                var markers2 = [];
                 var helsinki = {lat: 60.1699, lng: 24.9384};
-                var infowindow = new google.maps.InfoWindow({
-                    content: "Haloo toimiiks tää?"
-                });
+                var infowindow = new google.maps.InfoWindow();
+
                 var map = new google.maps.Map(document.getElementById('map'), {
-                    zoom: 10,
-                    center: helsinki
+                    zoom: 14,
+                    center: helsinki,
+                    type: 'restaurant'
                 });
-                var marker = new google.maps.Marker({
-                    position: helsinki,
-                    map: map,
-                    title:"Moi"
+
+                var service = new google.maps.places.PlacesService(map);
+                service.nearbySearch({
+                    location: helsinki,
+                    radius: 5000,
+                    type: ['restaurant']
+                }, callback);
+
+                function callback(results, status) {
+                    if (status === google.maps.places.PlacesServiceStatus.OK) {
+                    for (var i = 0; i < results.length; i++) {
+                        createMarker(results[i]);
+                    }
+                    }
+                }
+                 function createMarker(place) {
+                    var placeLoc = place.geometry.location;
+                    var marker = new google.maps.Marker({
+                        map: null,
+                        position: place.geometry.location
+                    });
+                    markers.push(marker);
+
+                    google.maps.event.addListener(marker, 'click', function() {
+                        infowindow.setContent(place.name);
+                        infowindow.open(map, this);
+                    });
+                }
+                function setMapClose(map, x) {
+                    if(x==1){
+                        y=markers;
+                    }
+                    else if(x==2){
+                        y=markers2;
+                    }
+                    for (var i = 0; i < y.length; i++) {
+                    y[i].setMap(map);
+                    }
+                }
+                 function clearMarkers(x) {
+                    setMapClose(null, x);
+                }
+                function showMarkers(x) {
+                    setMapClose(map, x);
+                }
+                var options = {
+                    types: ['establishment']
+                };
+
+                var input = document.getElementById('mapinput');
+                var autocomplete = new google.maps.places.Autocomplete(input, options);
+                map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+                autocomplete.addListener('place_changed', function() {
+                    var place = autocomplete.getPlace();
+                     var marker = new google.maps.Marker({
+                        map: map,
+                        position: place.geometry.location
+                    });
+                    markers2.push(marker);
+                    map.setCenter(marker.getPosition());  
+
                 });
-                 marker.addListener('click', function() {
-          infowindow.open(map, marker);
-        });
 
 
             }
